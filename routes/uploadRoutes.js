863c0830c5a1970bce6fa576630c909b6ed80888
umedzhan <umedzhan@kali.kali>
2026-02-10 23:36:47 +0500

    fix upload

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const express = require('express');
 const multer = require('multer');
+const fs = require('fs');
 const router = express.Router();
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, 'uploads/');
+        const uploadPath = 'uploads/';
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+        cb(null, uploadPath);
     },
     filename(req, file, cb) {
         cb(
